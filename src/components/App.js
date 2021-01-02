74d7f581657005fd2ced50c340e58aeba9801831
Jason <jason@Macintosh.local>
2021-01-02 10:42:27 -0600

    fetch data.json and setState

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,47 @@
 import "../css/App.css";
+import React, { Component } from "react";
 import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+    };
+  }
 
-  
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+  componentDidMount(){
+      fetch("./data.json")
+        .then((response) => response.json())
+        .then((result) => {
+          const apts = result.map((item) => {
+            return item;
+          });
+          this.setState({
+            myAppointments: apts,
+          });
+        });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
