01c2b7bacbb773148f3d47aad129fc3a08e71704
Jason <jason@Macintosh.local>
2021-01-02 11:08:47 -0600

    add key to each elements

--- src/components/App.js
+++ src/components/App.js
@@ -9,34 +9,35 @@ class App extends Component {
     super();
     this.state = {
       myAppointments: [],
+      lastIndex: 0,
     };
   }
 
-  componentDidMount(){
-      fetch("./data.json")
-        .then((response) => response.json())
-        .then((result) => {
-          const apts = result.map((item) => {
-            return item;
-          });
-          this.setState({
-            myAppointments: apts,
-          });
+  componentDidMount() {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((result) => {
+        const apts = result.map((item) => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
         });
+        this.setState({
+          myAppointments: apts,
+        });
+      });
   }
 
   render() {
-  
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-    
                 <AddAppointments />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments}/>
+                <ListAppointments appointments={this.state.myAppointments} />
               </div>
             </div>
           </div>
