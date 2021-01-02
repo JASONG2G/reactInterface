1097e002066d8e1a10cdfe8543320518da80ee65
Jason <jason@Macintosh.local>
2021-01-02 09:17:54 -0600

    added dependancies

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jason's react app.
         </a>
       </header>
     </div>
