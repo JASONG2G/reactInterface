0ab8210a00af1870d774134b3802ca295f0dae1a
Jason <jason@Macintosh.local>
2021-01-02 10:56:51 -0600

    passing data through props

--- src/components/App.js
+++ src/components/App.js
@@ -26,15 +26,17 @@ class App extends Component {
   }
 
   render() {
+  
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
+    
                 <AddAppointments />
                 <SearchAppointments />
-                <ListAppointments />
+                <ListAppointments appointments={this.state.myAppointments}/>
               </div>
             </div>
           </div>
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,6 +1,14 @@
-function ListAppointments() {
-        return (
-          <div>List Appointments</div>
-        )
-      }
-export  default ListAppointments;
+import React, { Component } from "react";
+
+class ListAppointments extends Component {
+  render() {
+    const listItems = this.props.appointments.map(item => (
+      <div>
+        <div>{item.petName}</div>
+        <div>{item.ownerName}</div>
+      </div>
+    ));
+    return <div>{listItems}</div>;
+  }
+}
+export default ListAppointments;
